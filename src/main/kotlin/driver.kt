fab5f51505f3d98af5b256e95f009e55711204d5
Mark McDonald <mark.mcdonald.ga@gmail.com>
2016-04-19 13:00:36 -0400

    Name block params over implicit 'it'

--- src/main/kotlin/driver.kt
+++ src/main/kotlin/driver.kt
@@ -4,7 +4,7 @@ fun main(args: Array<String>) {
     val cards = deck.cards.sortedBy { it.types.firstOrNull() }
     println("Your general is $general!")
 
-    val cardsWithQuantity = cards.groupBy(Card::name).mapValues { it.component2().size }
+    val cardsWithQuantity = cards.groupBy(Card::name).mapValues { cardsByName -> cardsByName.value.size }
     for ((name, quantity) in cardsWithQuantity) {
         println("$quantity: $name")
     }
